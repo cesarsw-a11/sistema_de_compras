608a3bf353b5b8750a1632301c3fcfcc4379018a
Cesar Meneses <cesarmeneses@localhost.localdomain>
2022-06-15 10:00:01 -0500

    [REF]Corregir el readme

--- application/views/head.php
+++ application/views/head.php
@@ -8,7 +8,7 @@
     <meta http-equiv="Pragma" content="no-cache">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
+s
 
     <!-- Title -->
     <title>Control Escolar Tecnica 22</title>
